a9bb543438e06358f5563ea2d1d8855f9f977e5f
khemirix <61617454+khemirix@users.noreply.github.com>
2020-06-09 14:43:12 +0100

    Add files via upload

--- src/tn/isetjb/servlet/BonsoirServlet.java
+++ src/tn/isetjb/servlet/BonsoirServlet.java
@@ -10,16 +10,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class BonjourServlet
+ * Servlet implementation class BonsoirServlet
  */
-@WebServlet("/BonjourServlet")
-public class BonjourServlet extends HttpServlet {
+@WebServlet("/Bonsoir")
+public class BonsoirServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public BonjourServlet() {
+    public BonsoirServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -27,28 +27,26 @@ public class BonjourServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-    int compteur=0;
-	protected void doGet(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
-
-		resp.setContentType("text/html");
-        resp.setCharacterEncoding("UTF-8");
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
         
-        PrintWriter out = resp.getWriter();
+        PrintWriter out = response.getWriter();
         out.println("<!DOCTYPE html>");
         out.println("<html>");
         out.println("<head>");
         out.println("<meta charset=\"utf-8\" />");
-        out.println("<title>Bonjour Iset</title>");
+        out.println("<title>Bonsoir Iset</title>");
         out.println("</head>");
         out.println("<body>");
-        out.println("<p>Bonjour Iset Djerba!</p>");
-        out.println("<p>Cette Servlet a été accédée " + compteur + " fois </p>");
+        out.println("<p>Bonsoir Iset Djerba!</p>");
+        
         out.println("</body>");
         out.println("</html>");
-        compteur++;
         
 	}
-	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
